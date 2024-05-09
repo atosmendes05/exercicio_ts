cf69155b0c0e0f81bad6173ff17be6156eda9b06
atos mendes <atosmendes48@gmail.com>
2024-05-08 21:20:38 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+function somar(a, b) {
+    return a * b;
+}
+console.log(somar(20, 25));
+function ola(nome) {
+    return `olá ${nome} tudo bem`;
+}
+console.log(ola('atos'));
---
+++ main.ts
@@ -0,0 +1,13 @@
+
+function somar(a:number,b:number){
+    return a * b
+}
+
+console.log(somar(20,25))
+
+
+function ola(nome:string){
+    return `olá ${nome} tudo bem`
+}
+
+console.log(ola('atos'))
